f18b667725e99fbf5e2eb06bbf186f44d803ca1f
Andre Blankholm <drebabydev@gmail.com>
2020-04-20 22:04:45 -0500

    daily2x

---
+++ countingCharacters/countingCharacters.js
@@ -0,0 +1,8 @@
+//Name: Andre Blankholm
+//Date: 4/20/2020
+//Last date revised: 4/20/2020
+
+
+function countingCharacters (stringToCount) { 
+	console.log (stringToCount + "has" + stringToCount.length + "characters." );
+	}
--- countingCharacters/countingChararters.js
+++
@@ -1,7 +0,0 @@
-
-//Name: Andre Blankholm
-//Date: 4/20/2020
-//Last date revised: 4/20/2020
-
-
-function countingCharacters (stringToCount) { console.log(stringToCount + "has" + stringToCount.length + "charaters" ); }
