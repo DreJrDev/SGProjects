d55bff46968d778c2fbf7d9bc1124c45183a8d69
Andre Blankholm <drebabydev@gmail.com>
2020-04-26 18:30:36 -0500

    daily

---
+++ countingCharacters/explorer.js
@@ -0,0 +1,20 @@
+// Name: Andre Blankholm
+// Date: 4/20/2020
+// Last date revised: 4/26/2020
+
+
+function countingCharacters3(str, search){ 
+	var count = 0; 
+	var numChars = search.length; 
+	var lastIndex = str.length - numChars; 
+		for (var index = 0; index <= lastIndex; index++){
+		     var current = str.substring(index, index + numChars); 
+		        if (current == search){                         
+                    count++;         
+        }     
+    }
+	
+	console.log("String to search in: " + str);
+    console.log("Character to find: " + search);
+    console.log("Number of times the character appears: " + count);
+}
