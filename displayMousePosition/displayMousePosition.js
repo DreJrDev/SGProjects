a37afa54d6463f411afcb34e667e35a6403a06d0
Andre Blankholm <drebabydev@gmail.com>
2020-07-19 21:55:31 -0500

    daily

--- displayMousePosition/displayMousePosition.js
+++ displayMousePosition/displayMousePosition.js
@@ -1,6 +1,42 @@
 //Name: Andre Blankholm
-//Date: 7/08/2020
-//Last date revised: 7/08/2020
+//Date: 7/16/2020
+//Last date revised: 7/19/2020
+
+
+
+var isMouseTracking = false;
+
+function updateMousePosition() {
+		if (isMouseTracking) {
+			var positionX = document.getElementById("mousePositionX");
+        positionX.innerText = event.clientX;
+        var positionY = document.getElementById("mousePositionY");
+        positionY.innerText = event.clientY;
+    }
+}
+
+function toggleTracking() {
+	isMouseTracking = !isMouseTracking;
+	if (isMouseTracking) {
+        document.getElementById("trackingStatus")
+            .innerText = "TRACKING";
+    } else {
+        document.getElementById("trackingStatus")
+            .innerText = "NOT TRACKING";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
